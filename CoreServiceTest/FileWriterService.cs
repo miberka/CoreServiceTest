64aea46d4ba8d480ac8760bbb5812bd7b77c76c0
Michal Berka <michal.berka@mainstream.cz>
2019-10-07 13:36:17 +0200

    Init Commit

---
+++ CoreServiceTest/FileWriterService.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreServiceTest
+{
+    public class FileWriterService : IHostedService, IDisposable
+    {
+        private const string Path = @"C:\Users\mberka\Desktop\TestApplication.txt";
+
+        private readonly ILogger logger = null;
+        private Timer timer;
+        private readonly IConfiguration configuration = null;
+    
+        public FileWriterService(ILogger<FileWriterService> logger, IConfiguration configuration)
+        {
+            this.logger = logger;
+            this.configuration = configuration;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            WriteTimeToFile("Started!");
+            this.timer = new Timer(
+                DoWork,
+                null,
+                TimeSpan.Zero,
+                TimeSpan.FromMinutes(1));
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            logger.LogInformation("Info log");
+            logger.LogInformation(configuration["prefix"]);
+            logger.LogDebug("Debug log");
+            logger.LogCritical("Critical log");
+            logger.LogError("Error log");
+            logger.LogWarning("Warning log");
+            using (logger.BeginScope("Starting scope", 25))
+            {
+                logger.LogInformation("Checking object state");
+
+                logger.LogInformation("Updating object reference");
+
+                logger.LogError("Something went wrong");
+            }
+            WriteTimeToFile("tick");
+        }
+
+        private void WriteTimeToFile(string message)
+        {
+            if (!File.Exists(Path))
+            {
+                using (var sw = File.CreateText(Path))
+                {
+                    sw.WriteLine($"{DateTime.UtcNow.ToString("O")} > {message}");
+                }
+            }
+            else
+            {
+                using (var sw = File.AppendText(Path))
+                {
+                    sw.WriteLine($"{DateTime.UtcNow.ToString("O")} > {message}");
+                }
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            WriteTimeToFile("Stopped!");
+            timer.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            timer?.Dispose();
+        }
+    }
+}
---
+++ CoreServiceTest/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration.CommandLine;
+using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
+namespace CoreServiceTest
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+
+            // https://www.stevejgordon.co.uk/running-net-core-generic-host-applications-as-a-windows-service
+
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+            var builder = new HostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    configApp.SetBasePath(Directory.GetCurrentDirectory());
+                    configApp.AddEnvironmentVariables(prefix: "TEST_");
+                    if (args != null)
+                    {
+                        configApp.AddCommandLine(args);
+                    }
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<FileWriterService>();
+                    services.AddLogging();
+                })
+                .ConfigureLogging((hostContext, logging) =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                    logging.AddEventLog();
+                    logging.AddDebug();
+                    logging.SetMinimumLevel(LogLevel.Debug);
+                    logging.AddEventSourceLogger();
+                });
+            if (isService)
+            {
+                await builder.RunAsServiceAsync();
+            }
+            else
+            {
+                await builder.RunConsoleAsync();
+            }
+        }
+    }
+}
